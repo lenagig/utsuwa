c728c4272667fb2fe1698c0b29eda5e7f2446cca
kakeru <kakeru.181115@gmail.com>
2026-09-27 01:12:28 +0900

    Add vessel ability types and battle data

--- src/types/vessel.ts
+++ src/types/vessel.ts
@@ -1,5 +1,22 @@
+export type VesselAbility =
+  | "poison"
+  | "burn"
+  | "attackSlow"
+  | "thunder"
+  | "doubleThrow"
+  | "lifeSteal"
+  | "reverse"
+  | "moveSlow"
+  | "defense"
+  | "debuffImmune"
+  | "invisible"
+  | "spectacle";
+
 export type Vessel = {
   attack: number;
+  ability: VesselAbility;
+  abilityLabel: string;
+  abilityDescription: string;
   id: string;
   name: string;
   imageUrl: string;
